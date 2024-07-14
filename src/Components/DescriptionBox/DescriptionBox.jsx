f0eaea18784b3b018775e5c0aba2ae5292f4af6d
KumarHarsh-hash <kumarharsh6691@gmail.com>
2024-07-14 20:59:08 +0530

    Initial commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const Description = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>E-commerce (electronic commerce) is the buying and selling of goods 
+           and services, or the transmitting of funds or data, over an electronic network, primarily the internet. -business are often used interchangeably. 
+           The term e-tail is also sometimes used in reference to the transactional processested to substantial growth in online retail. </p>
+           <p>
+           Many e-commerce apps use online marketing strategies to improve the cur types of outreach. They include emailing targeted ads and e-newsletters to subscribers and sending text messages to mobile devices. Sending unsolicited emails and texts is generally considered spam, so more companies now try to entice consumers online, using tools such as digital coupons, social media marketing and targeted advertisements.
+           </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Description
